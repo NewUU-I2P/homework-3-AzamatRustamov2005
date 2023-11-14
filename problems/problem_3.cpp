e221c07b73663e56fe1590be08cc3cb77dd7bb78
Rustamov Azamat <70460925+AzamatRustamov2005@users.noreply.github.com>
2023-11-14 13:25:53 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,8 +1,15 @@
 #include <string>
 
 std::string problemSolution3(float height, char S) {
-    // write your code here
-
-    // use return to return your result
-    // make use of control flow statements
+    std::string description;
+    float delta = S == 'M' ? 0 : 0.10;
+    
+    if (height + delta < 1.70)
+        description = "Short";
+    else if (height + delta < 1.85)
+        description = "Short";
+    else
+        description = "Short";
+    
+    return description;
 }
