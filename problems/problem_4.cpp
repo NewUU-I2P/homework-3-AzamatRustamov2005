25c4f606d61a6d9e752da475cc86c185af2dd164
Rustamov Azamat <70460925+AzamatRustamov2005@users.noreply.github.com>
2023-11-14 13:37:00 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -2,8 +2,23 @@
 #include <sstream>
 
 std::string problemSolution4(const std::string &macAddress) {
-    // write your code here
+    std::string result;
 
-    // make use of control flow statements
-    // return result;
+    bool is_ff = 1, is_even = 0;
+    
+    for (int i = 0; i < macAddress.size(); i++) {
+        is_ff &= macAddress.substr(i, 2) == "FF";
+    }
+
+    char parityDigit = macAddress[1];
+    if ('0' <= parityDigit <= '9')
+        is_even = (parityDigit - '0') % 2 == 0;
+    if ('A' <= parityDigit <= 'F')
+        is_even = (parityDigit - 'A') % 2 == 0;
+
+    if (typeA) result = "Broadcast";
+    else if (typeB) result = "Unicast";
+    else result = "Multicast";
+
+    return result;
 }
