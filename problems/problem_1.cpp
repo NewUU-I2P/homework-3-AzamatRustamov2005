63c27514f93e76cbdf134cf1a4568dfcf8d8977e
Rustamov Azamat <70460925+AzamatRustamov2005@users.noreply.github.com>
2023-11-14 13:58:57 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -6,11 +6,11 @@ float problemSolution1(float consumed_water) {
     cost = 13;
 
     float t1, t2, t3, t4;
-    t1 = std::min(30.0, consumed_water);
+    t1 = std::min(static_cast<float>(30), consumed_water);
     consumed_water -= t1;
-    t2 = std::min(20.0, consumed_water);
+    t2 = std::min(static_cast<float>(20), consumed_water);
     consumed_water -= t2;
-    t3 = std::min(10.0, consumed_water);
+    t3 = std::min(static_cast<float>(10), consumed_water);
     consumed_water -= t3;
     t4 = consumed_water;
     consumed_water -= t4;
