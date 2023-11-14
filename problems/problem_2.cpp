31994e83b6b41ed8db4feed9c4bd6125ffff00f1
Rustamov Azamat <70460925+AzamatRustamov2005@users.noreply.github.com>
2023-11-14 13:45:53 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -2,7 +2,7 @@ float problemSolution2(float i, float j, float k) {
     float result;
     
     result = i > j ? i : j;
-    result = result > k : result : k;
+    result = result > k ? result : k;
 
     return result;
 }
