0b62c6263ac27e6357381217d6767231ef10ed67
Rustamov Azamat <70460925+AzamatRustamov2005@users.noreply.github.com>
2023-11-14 13:20:52 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,19 @@
 void problemSolution1(float consumed_water) {
     float cost;
-    // write your code here
+
+    cost = 13;
+
+    int t1, t2, t3, t4;
+    t1 = min(30, consumed_water);
+    consumed_water -= t1;
+    t2 = min(20, consumed_water);
+    consumed_water -= t2;
+    t3 = min(10, consumed_water);
+    consumed_water -= t3;
+    t4 = consumed_water;
+    consumed_water -= t4;
+    
+    cost += 0.4 * t1 + 0.12 * t2 + 1.4 * t3 + 1.5 * t4;
 
     return cost;
 }
